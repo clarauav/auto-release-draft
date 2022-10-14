8aeb891f5e4bf5d7eabd10e2edbf7fb19f688bda
calvogoc <clara.calvo@roche.com>
2022-10-14 12:19:47 +0200

    added build

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
-import * as core from '@actions/core'
-import * as event from './event'
+import * as core from '@actions/core';
+import * as event from './event';
+import * as version from './version';
+import * as git from './git';
 
 async function run(): Promise<void> {
   try {
+    const tag = event.getCreatedTag();
+    if(tag && version.isSemver(tag)){
+      const changelog = await git.getChangesIntroducedByTag(tag);
+    }
     core.setOutput('release-url', 'https://example.com');
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message);
